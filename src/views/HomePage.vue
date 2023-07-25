46f6bb13ee2d8c007d85f004ec60828948480e7b
Todd H <github@simplybest.com>
2023-07-24 20:31:23 -0600

    cluttered wip

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,56 +1,187 @@
 <template>
   <ion-page>
     <ion-header :translucent="true">
-      <ion-toolbar>
-        <ion-title>Blank</ion-title>
+      <ion-toolbar color="primary">
+        <ion-title>Vue PWA Notify Me</ion-title>
       </ion-toolbar>
     </ion-header>
 
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
+    <ion-content>
 
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
+      <br>
+      <ion-list lines="none">
+        <!-- <ion-item>
+          <ion-button expand="block" :disabled="!isSupported" @click="getPermission" size="default">Request permission</ion-button>
+        </ion-item> -->
+        <ion-item>
+          <ion-input label="Message" label-placement="floating" v-model="message" :clear-input="true"></ion-input>
+          <ion-button slot="end" expand="block" :disabled="!isSupported" @click="notify" size="default">Notify
+            me</ion-button>
+        </ion-item>
+        <ion-item>
+          <ion-input type="number" label-placement="floating" label="Badge count" v-model="badgeCount"></ion-input>
+          <ion-button slot="end" expand="block" :disabled="!isSupported" @click="badgeCount = 0" size="default">
+            Clear
+            <ion-badge v-if="badgeCount" color="danger">{{ badgeCount }}</ion-badge>
+          </ion-button>
+        </ion-item>
+      </ion-list>
+      <br>
+      <br>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonInput, IonBadge, IonList, IonItem, onIonViewDidEnter, toastController } from '@ionic/vue';
+import { UseWebNotificationOptions, useWebNotification } from '@vueuse/core'
+import { ref, watch } from 'vue';
+
+const message = ref('Hello World!');
+const notificationCount = ref(0);
+const options = {
+  title: message.value,
+  dir: 'auto',
+  lang: 'en',
+  renotify: true,
+  tag: `tag-${notificationCount.value++}`,
+  vibrate: [200, 100, 200],
+} as UseWebNotificationOptions;
+const {
+  isSupported,
+  notification,
+  show,
+  close,
+  onClick,
+  onShow,
+  onError,
+  onClose,
+} = useWebNotification(options);
+
+onClick(event => {
+  console.log('Notification clicked', event);
+  toastController.create({
+    message: 'Notification clicked',
+    duration: 3000,
+  }).then(toast => toast.present());
+});
+
+const badgeCount = ref(3);
+const updateAppBadge = () => {
+  if (badgeCount.value === 0) {
+    navigator.clearAppBadge();
+  } else {
+    navigator.setAppBadge(badgeCount.value)
+  }
+};
+onIonViewDidEnter(() => {
+  updateAppBadge();
+});
+
+watch(badgeCount, updateAppBadge);
+
+const getPermission = (event: MouseEvent) => {
+  if (navigator.serviceWorker && window.PushManager && window.Notification) {
+
+    // navigator.serviceWorker.ready
+    // .then(registration => {
+    //     registration.pushManager.getSubscription()
+    //     .then(pushSubscription => {
+    //         if(!pushSubscription){
+    //             //the user was never subscribed
+    //             pushSubscription!.subscribe(registration);
+    //         }
+    //         else{
+    //             //check if user was subscribed with a different key
+    //             let json = pushSubscription.toJSON();
+    //             let public_key = json.keys.p256dh;
+                
+    //             console.log(public_key);
+                
+    //             if(public_key != NEW_PUBLIC_KEY){
+    //                 pushSubscription.unsubscribe().then(successful => {
+    //                     // You've successfully unsubscribed
+    //                     registration.pushManager.subscribe(registration);
+    //                 }).catch(e => {
+    //                     // Unsubscription failed
+    //                 })
+    //             }
+    //         }
+    //     });
+    // })
+
+    // Request permission to send push notifications
+    // navigator.serviceWorker.getRegistration()
+    //   .then(function(registration) {
+    //     registration!.pushManager.subscribe({ userVisibleOnly: true })
+    //       .then(function(subscription) {
+    //         console.log('Push notifications are allowed.');
+    //         //save the push subscription in your database
+    //       }).catch(function(error) {
+    //         console.log('Error:', error);
+    //       });
+    //   });
+  }
+  // try {
+  //   const result = await Notification.requestPermission();
+  //   console.log(result);
+  //   if (result === 'granted') {
+  //     show();
+  //   }
+  // } catch (e) {
+  //   console.error(e);
+  // }
+};
+
+const notify = async () => {
+  try {
+    await show();
+  } catch (e1) {
+    try {
+      notification.value = new Notification(message.value, {
+        ...options,
+        tag: `tag-${notificationCount.value++}`,
+      });
+    } catch (e2) {
+      // for chrome android
+      navigator.serviceWorker.ready.then((registration) => {
+        registration.showNotification("Vue Badger", {
+          actions: [
+            {
+              action: "close",
+              title: "Close",
+              icon: "/pwa-192x192.png",
+            },
+            {
+              action: "open",
+              title: "Open",
+              icon: "/pwa-192x192.png",
+            },
+          ],
+          body: message.value,
+          icon: "/pwa-192x192.png",
+          vibrate: [200, 100, 200],
+          badge: `${badgeCount.value}`,
+          tag: `tag-${notificationCount.value}`,
+        });
+      });
+    }
+  }
+};
 </script>
 
 <style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
+ion-badge {
+  --background: purple;
+  --color: white;
+  margin-left: 4px;
+  border-radius: 9999px;
+  margin-top: -8px;
+  min-width: 20px;
+  scale: 0.8;
 }
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
+ion-button {
+  min-width: 130px;
+  margin-top: 16px;
 }
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,38 @@ import legacy from '@vitejs/plugin-legacy'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    legacy()
+    legacy(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Vue Badger',
+        short_name: 'vBadger',
+        description: 'Vue PWA with notifications',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+    })
   ],
+  build: {
+    sourcemap: true,
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
