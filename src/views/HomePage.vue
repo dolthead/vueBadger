95b6d57d0decfbae26798de24b2de4b5744e6f22
Todd H <github@simplybest.com>
2023-08-02 19:26:05 -0600

    add message list UI

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -3,38 +3,34 @@
     <ion-header :translucent="true">
       <ion-toolbar color="primary">
         <ion-title>Vue PWA Notifications!</ion-title>
+        <ion-badge slot="end" color="danger">{{ badgeCount }}</ion-badge>
       </ion-toolbar>
     </ion-header>
 
     <ion-content>
 
-      <ion-list lines="none" class="zoom">
+      <ion-list lines="none">
 
         <ion-item>
-          <ion-input label="Message" label-placement="floating" v-model="message" :clear-input="true"></ion-input>
+          <ion-input label="Message" label-placement="floating" v-model="message" :clear-input="true" @keyup.enter="notify"></ion-input>
         </ion-item>
         <ion-button expand="block" :disabled="!supported" @click="notify" size="default">
           Send Message
         </ion-button>
+
         <ion-item-divider />
-        <ion-item>
-          <ion-fab-button slot="start" size="small" @click="updateBadgeCount(--badgeCount)">
-            <ion-icon :icon="remove"></ion-icon>
-          </ion-fab-button>
-          <ion-input class="elbow-room" type="number" label-placement="floating" label="Badge count" v-model="badgeCount"></ion-input>
-          <ion-fab-button slot="end" size="small" @click="updateBadgeCount(++badgeCount)">
-            <ion-icon :icon="add"></ion-icon>
+
+        <ion-item class="inbox">
+          <ion-label>Inbox</ion-label>
+          <ion-button slot="end" size="small" fill="outline" @click="markAllRead()">Mark all as read</ion-button>
+        </ion-item>
+
+        <ion-item v-for="message of messageList" :key="message.id" class="message" :class="{ 'read' : message.read }" button @click="markRead(message)">
+          <ion-label>{{ message.message }}</ion-label>
+          <ion-fab-button slot="end" size="small" color="white" fill="none" @click.stop="deleteMessage(message.id)">
+            <ion-icon size="small" color="danger" :icon="trash"></ion-icon>
           </ion-fab-button>
         </ion-item>
-        <ion-button expand="block" :disabled="!supported || !badgeCount" @click="updateBadgeCount(0)" size="default">
-          Clear, Mark All Read
-          <ion-badge v-if="badgeCount" color="danger">{{ badgeCount }}</ion-badge>
-        </ion-button>
-        
-        <!-- <ion-item v-for="message of messageList" :key="message.id" :class="{ 'read' : message.read }">
-          <ion-icon icon="notifications" slot="start"></ion-icon>
-          <ion-label>{{ message.message }}</ion-label>
-        </ion-item> -->
       </ion-list>
 
     </ion-content>
@@ -42,21 +38,21 @@
 </template>
 
 <script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonInput, IonBadge, IonFabButton, IonItemDivider, IonList, IonItem, IonIcon, toastController, onIonViewDidEnter } from '@ionic/vue';
+import { IonContent, IonHeader, IonPage, IonTitle, IonBadge, IonToolbar, IonButton, IonFabButton, IonInput, IonLabel, IonItemDivider, IonList, IonItem, IonIcon, toastController, onIonViewDidEnter } from '@ionic/vue';
 import { UseWebNotificationOptions, useWebNotification } from '@vueuse/core'
-import { add, remove } from 'ionicons/icons';
-import { ref, watch } from 'vue';
+import { trash } from 'ionicons/icons';
+import { ref } from 'vue';
 
 const message = ref('Hello World!');
-const notificationCount = ref(0);
-const badgeCount = ref(3);
-// const messageList = ref([
-//   { id: 1, message: 'Hello World!', read: false },
-//   { id: 2, message: 'Hello World!', read: false },
-//   { id: 3, message: 'Hello World!', read: false },
-//   { id: 4, message: 'Hello World!', read: true },
-// ]);
-// let nextId = 5;
+let notificationCount = 0;
+const messageList = ref([
+  { id: 1, message: 'Hello World!', read: false },
+  { id: 2, message: 'Hello World!', read: false },
+  { id: 3, message: 'Hello World!', read: false },
+  { id: 4, message: 'Hello World!', read: true },
+]);
+let nextId = 5;
+const badgeCount = ref(0);
 
 const options = () => ({
   title: message.value,
@@ -64,7 +60,8 @@ const options = () => ({
   dir: 'auto',
   lang: 'en',
   renotify: true,
-  tag: `tag-${notificationCount.value++}`,
+  // tag: 'vue-pwa-notification', // resend with same tag to replace previous notification
+  tag: `tag-${notificationCount++}`, // resend with unique tag to queue up many unique notifications
   icon: "/pwa-192x192.png",
   vibrate: [200, 100, 200],
 } as UseWebNotificationOptions);
@@ -98,14 +95,26 @@ onIonViewDidEnter(() => {
       .then((registration) => swRegistration = registration, console.error);
 });
 
-const updateBadgeCount = async (x: number) => {
-  badgeCount.value = x;
-  if (badgeCount.value < 0) {
-    badgeCount.value = 0;
+const deleteMessage = (id:number) => {
+  const index = messageList.value.findIndex(message => message.id === id);
+  if (index > -1) {
+    messageList.value.splice(index, 1);
   }
+  updateAppBadge();
+};
+
+const markRead = (message: any) => {
+  message.read = true;
+  updateAppBadge();
+};
+
+const markAllRead = () => {
+  messageList.value.forEach(message => message.read = true);
+  updateAppBadge();
 };
 
 const updateAppBadge = async () => {
+  badgeCount.value = messageList.value.filter(message => !message.read).length;
   if (badgeCount.value === 0) {
     navigator.clearAppBadge && await navigator.clearAppBadge();
   } else {
@@ -113,12 +122,9 @@ const updateAppBadge = async () => {
   }
 };
 
-watch(badgeCount, updateAppBadge);
-
 const notify = () => {
-  updateBadgeCount(++badgeCount.value);
-  // messageList.value.unshift({ id: nextId++, message: message.value, read: false });
-  // badgeCount.value++;
+  messageList.value.unshift({ id: nextId++, message: message.value, read: false });
+  updateAppBadge();
   const { show } = useWebNotification(options());
   if (swRegistration) {
     swRegistration.showNotification("Vue Badger", options());
@@ -131,29 +137,38 @@ const notify = () => {
 
 <style scoped>
 ion-badge {
-  --background: purple;
-  --color: white;
-  margin-left: 4px;
   border-radius: 9999px;
   margin-top: -8px;
   min-width: 20px;
-  scale: 0.8;
+  padding: 8px;
+  margin-inline-end: 16px;
 }
-
 ion-button {
   min-width: 130px;
   margin-inline: 12px;
 }
-
 ion-item-divider {
   border-bottom: none;
   background-color: white;
 }
-
-.zoom {
-  zoom: 1.4;
+.message {
+  & ::part(native) {
+    min-height: 32px !important;
+  }
+  & ion-label {
+    font-weight: bold;
+  }
 }
-.read {
+.message.read ion-label {
   color: gray;
+  font-weight: normal;
+}
+ion-fab-button {
+  --background: transparent;
+  --box-shadow: none;
+}
+.inbox ion-label {
+  font-weight: bold;
+  font-size: x-large;
 }
 </style>
